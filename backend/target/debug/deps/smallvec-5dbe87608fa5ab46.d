3a468ad8829acfd0429cc5be5f346e9c12eeb173
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:19:45 +0800

    Update project

---
+++ backend/target/debug/deps/smallvec-5dbe87608fa5ab46.d
@@ -0,0 +1,7 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\smallvec-5dbe87608fa5ab46.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libsmallvec-5dbe87608fa5ab46.rlib: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libsmallvec-5dbe87608fa5ab46.rmeta: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs:
