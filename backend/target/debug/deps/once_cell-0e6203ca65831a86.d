f49f3f4e097651ab2b2f03cee78c221b9006ecbc
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:19:40 +0800

    Update project

---
+++ backend/target/debug/deps/once_cell-0e6203ca65831a86.d
@@ -0,0 +1,9 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\once_cell-0e6203ca65831a86.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\imp_std.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\race.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libonce_cell-0e6203ca65831a86.rlib: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\imp_std.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\race.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libonce_cell-0e6203ca65831a86.rmeta: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\imp_std.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\race.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\lib.rs:
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\imp_std.rs:
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\once_cell-1.21.4\src\race.rs:
