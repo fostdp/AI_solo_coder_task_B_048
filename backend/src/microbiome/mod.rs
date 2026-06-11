4f76efa6950e7947ae58f75186f00d954d5c849d
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:20:00 +0800

    Update project

--- backend/src/microbiome/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod random_forest;
-pub mod microbiome_data;
-
-pub use random_forest::{MicrobeCorrelationAnalyzer, CorrelationResult, FeatureImportance};
-pub use microbiome_data::{MicrobiomeSample, MicrobeAbundance, FunctionalGene};
