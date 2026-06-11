16860eb05c4193d51fc8d637c92fda986a257426
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:19:42 +0800

    Update project

---
+++ backend/target/debug/deps/pin_project_lite-c9545d3e16671299.d
@@ -0,0 +1,7 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\pin_project_lite-c9545d3e16671299.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libpin_project_lite-c9545d3e16671299.rlib: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libpin_project_lite-c9545d3e16671299.rmeta: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs:
