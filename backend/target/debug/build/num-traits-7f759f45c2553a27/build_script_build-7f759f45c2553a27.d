e3b69e2d624bf61146e6e4bf7898148d6f8fb2b7
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:17:24 +0800

    Update project

---
+++ backend/target/debug/build/num-traits-7f759f45c2553a27/build_script_build-7f759f45c2553a27.d
@@ -0,0 +1,5 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\build\num-traits-7f759f45c2553a27\build_script_build-7f759f45c2553a27.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\build\num-traits-7f759f45c2553a27\build_script_build-7f759f45c2553a27.exe: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs:
