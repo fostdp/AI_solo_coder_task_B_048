160226d604f7661aafc4e87f8a567214e736b3b8
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:19:54 +0800

    Update project

--- backend/src/groundwater/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod modflow_simple;
-pub mod chloride_transport;
-
-pub use modflow_simple::{GroundwaterModel, FlowFieldResult, WellPoint, GridCell};
-pub use chloride_transport::{ChlorideTransport, DiffusionResult, ContaminationPath};
