5412977e49f94905c6bdc1b3489ae722cf12052b
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:17:28 +0800

    Update project

---
+++ backend/target/debug/deps/cfg_if-50da2642d7d10359.d
@@ -0,0 +1,7 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\cfg_if-50da2642d7d10359.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libcfg_if-50da2642d7d10359.rlib: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libcfg_if-50da2642d7d10359.rmeta: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
