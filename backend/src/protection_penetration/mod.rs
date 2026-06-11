fb8ac86f625597825721484578759336ca6b1150
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:20:05 +0800

    Update project

--- backend/src/protection_penetration/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod fick_diffusion;
-pub mod materials;
-
-pub use fick_diffusion::{PenetrationSimulator, PenetrationResult, PenetrationProfile};
-pub use materials::{ProtectiveMaterial, MaterialType};
