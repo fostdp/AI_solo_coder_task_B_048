62311b275288fb7403dbfd4c030c45c7c8e63e9e
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:19:50 +0800

    Update project

---
+++ backend/target/debug/deps/windows_link-d2ef63ecef51002c.d
@@ -0,0 +1,8 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\windows_link-d2ef63ecef51002c.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libwindows_link-d2ef63ecef51002c.rlib: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\deps\libwindows_link-d2ef63ecef51002c.rmeta: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\lib.rs:
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows-link-0.2.1\src\../readme.md:
