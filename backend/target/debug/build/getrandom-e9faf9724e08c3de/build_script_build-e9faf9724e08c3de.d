65a51df84d4b6c8ac3e3bcb9c1368e9e9ca29ebf
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 10:17:21 +0800

    Update project

---
+++ backend/target/debug/build/getrandom-e9faf9724e08c3de/build_script_build-e9faf9724e08c3de.d
@@ -0,0 +1,5 @@
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\build\getrandom-e9faf9724e08c3de\build_script_build-e9faf9724e08c3de.d: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.4.2\build.rs
+
+D:\SOLO-2\AI_solo_coder_task_B_048\backend\target\debug\build\getrandom-e9faf9724e08c3de\build_script_build-e9faf9724e08c3de.exe: C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.4.2\build.rs
+
+C:\Users\y\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.4.2\build.rs:
